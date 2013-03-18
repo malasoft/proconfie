6c49a1d7149405263f6a2a9e6884f38c1972f4c1
eduardom <edumucelli@gmail.com>
2013-03-18 09:59:02 +0100

    Update secret_token.rb
    Hiding the secret_token!

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Proconfie3::Application.config.secret_token = 'da14ad4ebdebf70f69dfc2bf4fdc932bb5bdd655fe30f2ba6715c9a29c2673bf85ec69c771f87f017649a0b0607968722f33e395db45e04fb0ded0be1aafc051'
+Proconfie3::Application.config.secret_token = 'secret_token_goes_here'
